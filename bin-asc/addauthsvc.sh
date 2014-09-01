d13d0990240aebfb7d5ba7fe24bfc6e19afceb87
root <root@jay007.eng.platformlab.ibm.com>
2014-09-01 03:07:15 -0400

    update

--- bin-asc/addauthsvc.sh
+++ bin-asc/addauthsvc.sh
@@ -11,6 +11,8 @@ dns_search="--dns-search `echo $dns_search_list | sed "s/ / --dns-search /g"`"
  
 docker_cmd="docker"
 
+as_suffix=-liberty
+
 max=$($docker_cmd ps -a | grep 'auth[0-9]\+ *$' | sed 's/.*auth\([0-9]\+\).*/\1/' | sort -n | tail -n 1)
 num=$(expr $max + 1)
 
@@ -20,7 +22,7 @@ $docker_cmd run \
 --dns "$dns2" \
 --dns "$dns3" \
 $dns_search \
---name auth$num -h auth$num.auth-service${as_suffix}.local.flyacmeair.net \
+--name auth${EGO_CONTAINER_ID} -h auth${EGO_CONTAINER_ID}.auth-service${as_suffix}.local.flyacmeair.net \
 acmeair/auth-service${as_suffix}
 
 while [ 1 ];
--- bin-asc/addwebapp.sh
+++ bin-asc/addwebapp.sh
@@ -15,13 +15,15 @@ docker_cmd="docker"
 max=$($docker_cmd ps -a | grep 'webapp[0-9]\+ *$' | sed 's/.*webapp\([0-9]\+\).*/\1/' | sort -n | tail -n 1)
 num=$(expr $max + 1)
 
+as_suffix=-liberty
+
 $docker_cmd run \
 -d -t -P \
 --dns "$dns1" \
 --dns "$dns2" \
 --dns "$dns3" \
 $dns_search \
---name webapp$num -h webapp$num.webappi${as_suffix}.local.flyacmeair.net \
+--name webapp${EGO_CONTAINER_ID} -h webapp${EGO_CONTAINER_ID}.webappi${as_suffix}.local.flyacmeair.net \
 acmeair/webapp${as_suffix}
 
 while [ 1 ];
